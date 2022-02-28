b8611cd04df63cf1ed3957ba8bfe4b0ade0ee28b
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-02-28 10:52:03 +0530

    auto pushing chainges repo

--- seed.groovy
+++ seed.groovy
@@ -25,13 +25,14 @@ def component = ["cart", "catalogue", "user", "frontend", "shipping", "payment"]
 def count = (component.size() -1 )
 
 for(int i in 0..count) {
-  pipelineJob("CI-Pipelines/${component[i]}") {
+  def j = component[i]
+  pipelineJob("CI-Pipelines/${j}") {
     configure { flowdefinition ->
       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'("https://manjusha9722@dev.azure.com/manjusha9722/DevOps/_git/${component[i]}")
+              'url'("https://manjusha9722@dev.azure.com/manjusha9722/DevOps/_git/${j}")
             }
           }
           'branches' {
