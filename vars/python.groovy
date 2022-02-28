8b44ec98c1bbb65731a3446b724d556f17d8b3ce
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-02-28 11:34:35 +0530

    auto pushing chainges repo

---
+++ vars/python.groovy
@@ -0,0 +1,58 @@
+def call(String COMPONENT){
+
+    pipeline{
+
+        agent {
+            node {
+                label "PYTHON"
+            }
+        }
+        environment {
+            sonar_token = credentials('sonar_token')
+        }
+        stages {
+
+            stage('Check code quality') {
+                steps {
+                    sh """
+                    sonar-scanner  -Dsonar.java.binaries=target/. -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.19.162:9000 -Dsonar.login=${sonar_token}
+                  """
+                }
+            }
+
+            stage('link checks'){
+                steps {
+                    echo 'Link Checks'
+                }
+            }
+
+            stage('unit tests'){
+                steps {
+                    echo 'unit tests'
+                }
+            }
+
+            stage('Prepare artifact') { // artifact is a piece getting ready for a file to get downloaded
+                steps {
+                    sh """
+                 cd static
+                 zip -r ${COMPONENT}.zip *
+               """
+                }
+            }
+
+            stage('Publish artifacts'){
+                steps {
+                    echo 'Publish artifacts'
+                }
+            }
+        }
+
+        post {
+            // Clean after build
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
