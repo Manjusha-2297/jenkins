5a7bcdbe47e77d5d4988c228131580aecb4251e2
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2022-03-19 18:09:16 +0530

    push make

--- seed.groovy
+++ seed.groovy
@@ -127,3 +127,25 @@ for(int i in 0..count) {
   }
 }
 
+pipelineJob("Mutable/Destroy") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+      'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://github.com/Manjusha-2297/terraform-mutable.git")
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('jenkinsfile-destroy')
+      'lightweight'(true)
+    }
+  }
+}
+
+
